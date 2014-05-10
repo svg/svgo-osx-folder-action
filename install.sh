1e1a3fb5b50130e3a7bd494447b5ee074cd7a690
Scott Messinger <scottmessinger@gmail.com>
2014-05-09 21:29:36 -0400

    Update URL to svgo.applescript

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@
 SCRIPTS_PATH="$HOME/Library/Scripts/Folder Action Scripts"
 
 mkdir -p "$SCRIPTS_PATH"
-curl -sSO https://raw.github.com/svg/svgo-osx-folder-action/master/svgo.applescript
+curl -sSO https://raw.githubusercontent.com/svg/svgo-osx-folder-action/master/svgo.applescript
 osacompile -o "$SCRIPTS_PATH/svgo.scpt" svgo.applescript
 rm svgo.applescript
